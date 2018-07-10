3fdf2433d9cee0be25d03443d9e09431dd24d7ca
Laura <laclark@mitre.org>
2018-07-10 10:43:40 -0400

    new cqm validators library

---
+++ cqm_validators/lib/cqm_validators.rb
@@ -0,0 +1,5 @@
+require "cqm_validators/version"
+
+module CqmValidators
+  # Your code goes here...
+end
---
+++ cqm_validators/test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "cqm_validators"
+
+require "minitest/autorun"
