398273f7eb3339d676079a3ffae1ecfe2ae95696
Laura <laclark@mitre.org>
2018-07-17 07:33:48 -0400

    added all dependicies needed for cypress

---
+++ lib/base_validator.rb
@@ -0,0 +1,22 @@
+module CqmValidators
+      module BaseValidator
+  
+        def build_error(msg, loc, file_name)
+          ValidationError.new(message: msg, location: loc, file_name: file_name, validator: @name)
+        end
+  
+        def get_document(input)
+          doc = case input
+          when File
+            input.read
+          when Nokogiri::XML::Document
+            return input
+          else
+            input
+          end
+          Nokogiri::XML(doc.to_s) { |conf| conf.strict.nonet.noblanks } #grumble, grumble nokogiri java @SS 
+        end
+      end
+  
+end
+  
--- lib/cqm_validators.rb
+++ lib/cqm_validators.rb
@@ -1,4 +1,14 @@
 require "cqm_validators/version"
+require 'reported_result_extractor'
+require 'base_validator'
+require 'data_validator'
+require 'measure_validator'
+require 'performance_rate_validator'
+require 'qrda_qdm_template_validator'
+require 'schema_validator'
+require 'schematron_validator'
+require 'validators'
+require "schematron/c_processor"
 
 module CqmValidators
   # Your code goes here...
