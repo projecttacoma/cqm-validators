0ae70e430c5fb9b7edae5cb36137dd42073e220b
Michael O'Keefe <mokeefe@mitre.org>
2019-02-12 12:49:50 -0500

    Initial Rubocop fix commit, .todo seems buggy though

--- lib/base_validator.rb
+++ lib/base_validator.rb
@@ -1,22 +1,21 @@
+# frozen_string_literal: true
+
 module CqmValidators
-      module BaseValidator
-  
-        def build_error(msg, loc, file_name)
-          ValidationError.new(message: msg, location: loc, file_name: file_name, validator: @name)
-        end
-  
-        def get_document(input)
-          doc = case input
-          when File
-            input.read
-          when Nokogiri::XML::Document
-            return input
-          else
-            input
-          end
-          Nokogiri::XML(doc.to_s) { |conf| conf.strict.nonet.noblanks } #grumble, grumble nokogiri java @SS 
-        end
-      end
-  
+  module BaseValidator
+    def build_error(msg, loc, file_name)
+      ValidationError.new(message: msg, location: loc, file_name: file_name, validator: @name)
+    end
+
+    def get_document(input)
+      doc = case input
+            when File
+              input.read
+            when Nokogiri::XML::Document
+              return input
+            else
+              input
+            end
+      Nokogiri::XML(doc.to_s) { |conf| conf.strict.nonet.noblanks } # grumble, grumble nokogiri java @SS
+    end
+  end
 end
-  
