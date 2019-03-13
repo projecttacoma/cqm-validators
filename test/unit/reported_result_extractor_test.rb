402c7dfec634fd1a63f6ae0097104a80c0a075b7
Dave Czulada <dczulada@mitre.org>
2019-03-13 13:14:34 -0400

    Add test for stratification cv

--- test/unit/reported_result_extractor_test.rb
+++ test/unit/reported_result_extractor_test.rb
@@ -20,4 +20,15 @@ class ReportedResultExtractorTest < Minitest::Test
     # make sure the OBSERV result (the actual CV value) equals the value from the XML
     assert_equal results['OBSERV'], 15.0
   end
+
+  def test_should_return_the_correct_reported_result_for_a_stratified_cv_value
+    doc = File.new('test/fixtures/qrda/cat3_cv_good.xml')
+    measure = CQM::Measure.where(hqmf_id: '40280382-5FA6-FE85-015F-BB40A1CD0B95').first
+    doc = Nokogiri::XML(doc)
+    doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
+    results = extract_results_by_ids(measure, 'PopulationCriteria1', doc, 'PopulationCriteria1 - Stratification 3')
+
+    # make sure the OBSERV result (the actual CV value) equals the value from the XML
+    assert_equal results['OBSERV'], 15.0
+  end
 end
