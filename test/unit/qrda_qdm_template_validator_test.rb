83e913d2fb0557b196335e8c5b351500eb666f78
Dave Czulada <dczulada@mitre.org>
2019-03-13 10:37:27 -0400

    update test suite

--- test/unit/qrda_qdm_template_validator_test.rb
+++ test/unit/qrda_qdm_template_validator_test.rb
@@ -6,25 +6,25 @@ class QrdaQdmTemplateValidatorTest < Minitest::Test
 
   def setup
     @validator_r3 = CqmValidators::QrdaQdmTemplateValidator.new('r3')
-    @validator_r31 = CqmValidators::QrdaQdmTemplateValidator.new('r3_1')
+    @validator_r5 = CqmValidators::QrdaQdmTemplateValidator.new('r5')
     @validator_r6 = CqmValidators::QrdaQdmTemplateValidator.new('r6')
   end
 
   def test_should_not_produce_errors_if_validator_does_not_support_version_specified
-    xml = File.open('./test/fixtures/qrda/cat1_r3_good.xml', 'r', &:read)
+    xml = File.open('./test/fixtures/qrda/cat1_good.xml', 'r', &:read)
     errors = @validator_r6.validate(xml)
     assert_equal 0, errors.length, 'File should not contain any errors'
   end
 
   def test_should_not_produce_errors_for_good_file
-    xml = File.open('./test/fixtures/qrda/cat1_r3_good.xml', 'r', &:read)
-    errors = @validator_r3.validate(xml)
+    xml = File.open('./test/fixtures/qrda/cat1_good.xml', 'r', &:read)
+    errors = @validator_r5.validate(xml)
     assert_equal 0, errors.length, 'File should not contain any errors'
   end
 
   def test_should_produce_4_errors_for_using_r3_templates_in_an_r3_1_document
-    xml = File.open('./test/fixtures/qrda/cat1_r3_good.xml', 'r', &:read)
-    errors = @validator_r31.validate(xml)
-    assert_equal 4, errors.length, 'File should contain 4 errors for incorrect templates'
+    xml = File.open('./test/fixtures/qrda/cat1_good.xml', 'r', &:read)
+    errors = @validator_r3.validate(xml)
+    assert_equal 1, errors.length, 'File should contain 1 errors for incorrect templates'
   end
 end
--- test/unit/reported_result_extractor_test.rb
+++ test/unit/reported_result_extractor_test.rb
@@ -6,15 +6,18 @@ require 'fileutils'
 class ReportedResultExtractorTest < Minitest::Test
   include CqmValidators::ReportedResultExtractor
 
+  def setup
+    collection_fixtures('cqm_measures')
+  end
+
   def test_should_return_the_correct_reported_result_for_a_cv_value
     doc = File.new('test/fixtures/qrda/cat3_cv_good.xml')
-    id = '40280381-4C18-79DF-014C-291EF3F90654'
-    ids = { 'IPP' => 'EAD808CB-A6FA-4824-A204-74F299839396', 'MSRPOPL' => '7462E67A-5ECB-41D6-AE14-2E89BB55BBDE', 'OBSERV' => '2D084067-703B-4072-9F43-D50F938F4F9C' }
-
+    measure = CQM::Measure.where(hqmf_id: '40280382-5FA6-FE85-015F-BB40A1CD0B95').first
     doc = Nokogiri::XML(doc)
-    results = extract_results_by_ids(id, ids, doc)
+    doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
+    results = extract_results_by_ids(measure, 'PopulationCriteria1', doc)
 
     # make sure the OBSERV result (the actual CV value) equals the value from the XML
-    assert_equal results['OBSERV'], 240.0
+    assert_equal results['OBSERV'], 15.0
   end
 end
