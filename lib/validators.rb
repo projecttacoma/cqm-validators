3d601b056203c75b531585675590b152bc7a9a2e
Dave Czulada <dczulada@mitre.org>
2019-03-15 14:24:44 -0400

    add QRDA Cat I R51.1 schematron

--- lib/validators.rb
+++ lib/validators.rb
@@ -12,6 +12,7 @@ require_relative 'qrda_qdm_template_validator'
 module CqmValidators
   CDA_SDTC_SCHEMA = 'lib/schema/infrastructure/cda/CDA_SDTC.xsd'
   QRDA_CAT1_R5_SCHEMATRON = 'lib/schematron/qrda/cat_1_r5/HL7 QRDA Category I STU 5.sch'
+  QRDA_CAT1_R51_SCHEMATRON = 'lib/schematron/qrda/cat_1_r5_1/HL7 QRDA Category I STU 5.1.sch'
   QRDA_CAT3_21SCHEMATRON = 'lib/schematron/qrda/cat_3_r2_1/HL7 QRDA Category III STU 2.1.sch'
   BASE_DIR = File.expand_path('..', __dir__)
 
@@ -44,6 +45,14 @@ module CqmValidators
 
     def initialize
       super('QRDA Cat 1 Validator', File.join(BASE_DIR, QRDA_CAT1_R5_SCHEMATRON))
+    end
+  end
+
+  class Cat1R51 < Schematron::Validator
+    include Singleton
+
+    def initialize
+      super('QRDA Cat 1 Validator', File.join(BASE_DIR, QRDA_CAT1_R51_SCHEMATRON))
     end
   end
 
