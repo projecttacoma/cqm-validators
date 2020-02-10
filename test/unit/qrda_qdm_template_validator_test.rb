47ebfb40683da1059f32ad84c1ddcd33f7d11086
Dave Czulada <dczulada@mitre.org>
2020-02-10 11:51:19 -0500

    update template ids in known good to use QRDA Cat I R5.2

--- test/unit/qrda_qdm_template_validator_test.rb
+++ test/unit/qrda_qdm_template_validator_test.rb
@@ -6,7 +6,7 @@ class QrdaQdmTemplateValidatorTest < Minitest::Test
 
   def setup
     @validator_r5 = CqmValidators::QrdaQdmTemplateValidator.new('r5')
-    @validator_r51 = CqmValidators::QrdaQdmTemplateValidator.new('r5_1')
+    @validator_r52 = CqmValidators::QrdaQdmTemplateValidator.new('r5_2')
     @validator_r6 = CqmValidators::QrdaQdmTemplateValidator.new('r6')
   end
 
@@ -18,13 +18,13 @@ class QrdaQdmTemplateValidatorTest < Minitest::Test
 
   def test_should_not_produce_errors_for_good_file
     xml = File.open('./test/fixtures/qrda/cat1_good.xml', 'r', &:read)
-    errors = @validator_r51.validate(xml)
+    errors = @validator_r52.validate(xml)
     assert_equal 0, errors.length, 'File should not contain any errors'
   end
 
   def test_should_produce_1_error_for_using_r5_1_templates_in_an_r5_document
     xml = File.open('./test/fixtures/qrda/cat1_good.xml', 'r', &:read)
     errors = @validator_r5.validate(xml)
-    assert_equal 1, errors.length, 'File should contain 1 errors for incorrect templates'
+    assert_equal 2, errors.length, 'File should contain 1 errors for incorrect templates'
   end
 end
