48f38b5723ad2b0cfa2d6a8bf571d39cb6aa13b1
lmd59 <laurend@mitre.org>
2021-01-15 15:12:42 -0500

    codecov updates

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
-
 require 'simplecov'
+SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'cqm_validators'
